c2b0bb5aaa3b1e8fd73c53e77ecab8973595c434
dima <dmitrii.bushmelev@gmail.com>
2019-10-14 08:28:15 +0200

    Add hash prepending to a building script

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
+
 mkdir -p build
 rm -rf build/*
 
-cp src/index.html build/index.html
-
 npm run build-dev
+
+HASH=$(md5sum build/index.js | awk '{print $1}')
+FILE=$HASH.js
+
+cp build/index.js "build/$FILE"
+sed "s/index.js/$FILE/g" src/index.html > build/index.html
